98b0a4cdfa993bd29c312e0abe531f192c5614c4
wandzi <wandzel.mateusz@yahoo.com>
2020-02-23 11:09:23 +0100

    Fix(API): PATCH request is working with handling errors.

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './Form.scss';
 import axios from 'axios';
+import './Form.scss';
 
 class Form extends Component {
     constructor(props){
         super(props);
         this.state = {
-            id: this.props.gnomeId,
+            id: props.gnomeId,
             submitedName: props.gnomeName,
             submitedAge: props.gnomeAge,
             submitedStrength: props.gnomeStrength,
@@ -25,6 +25,33 @@ class Form extends Component {
        this.setState({ validationMessage });
     }
 
+    formValidation(gnome) {
+        // Color of validation message
+        if (gnome.name.length === 0 || gnome.age.length === 0 || gnome.strenght.length === 0) {
+            this.handleValidationMessage('You should fill all the inputs.');
+        } else if (gnome.name === this.props.gnomeName && gnome.age === this.props.gnomeAge && gnome.strenght === this.props.gnomeStrength) {
+            this.handleValidationMessage('You should edit Gnome before submitting.');
+        } else if ( gnome.age < 0 || gnome.age > 100 ) { 
+            this.handleValidationMessage('Gnome age should be from 0 to 100');
+        } else if ( gnome.strenght < 0 || gnome.strenght > 100 ) { 
+            this.handleValidationMessage('Gnome strength should be from 0 to 100');
+        } else {
+        
+            axios({
+                url: `http://master.datasource.jazzy-hr.jzapp.io/api/v1/gnomes/${this.state.id}`,
+                data: {gnome},
+                method: 'PATCH',
+            })
+            .then( res => {
+                (res.status === 200) ? this.handleValidationMessage('Gnome edited succesfully.') : this.handleValidationMessage('Request problem with editing Gnome.');
+            })
+            .catch( error => {
+                this.handleValidationMessage(`Request problem with editing Gnome. - ${error}`);
+            });   
+
+        }     
+    }
+
     onSubmit(event) {
         event.preventDefault();
         
@@ -34,29 +61,11 @@ class Form extends Component {
                 strenght: this.state.submitedStrength,
               }
 
-        // Color of validation message
-        if (gnome.name.length === 0 || gnome.age.length === 0 || gnome.strenght.length === 0) {
-            this.handleValidationMessage('You should fill all the inputs.');
-        } else if ( gnome.age < 0 || gnome.age > 100 ) { 
-            this.handleValidationMessage('Gnome age should be from 0 to 100');
-        } else if ( gnome.strenght < 0 || gnome.strenght > 100 ) { 
-            this.handleValidationMessage('Gnome strength should be from 0 to 100');
-        } else {
-        
-            axios.post(`http://master.datasource.jazzy-hr.jzapp.io/api/v1/gnomes/${this.state.id}`, {gnome})
-            .then( res => {
-                console.log(res);
-                console.log(res.data);
-                this.handleValidationMessage('Gnome edited succesfully.');
-            })
-            .catch( error => {
-                this.handleValidationMessage('Request problem with editing Gnome.');
-                console.log(error);
-            });   
-        }     
+        this.formValidation(gnome);
     }
 
     render() {
+
         let validationMessageColor = null;
 
         switch(this.state.validationMessage) {
@@ -75,6 +84,9 @@ class Form extends Component {
             case 'Request problem with editing Gnome.':
                 validationMessageColor={color: `#FF0000`};
                 break;
+            case 'You should edit Gnome before submitting.':
+                validationMessageColor={color: `#FF0000`};
+                break;
             default: validationMessageColor = null;
         }
 
@@ -82,15 +94,30 @@ class Form extends Component {
             <form onSubmit={this.onSubmit}>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="submitedName" id="name" defaultValue={this.props.gnomeName} onChange={this.handleChange}/>
+                    <input 
+                    type="text" 
+                    name="submitedName" 
+                    id="name" 
+                    defaultValue={this.props.gnomeName} 
+                    onChange={this.handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="age">Age</label>
-                    <input type="text" name="submitedAge" id="age" defaultValue={this.props.gnomeAge} onChange={this.handleChange}/>
+                    <input 
+                    type="text" 
+                    name="submitedAge" 
+                    id="age" 
+                    defaultValue={this.props.gnomeAge} 
+                    onChange={this.handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="strength">Strength</label>
-                    <input type="text" name="submitedStrength" id="strength" defaultValue={this.props.gnomeStrength} onChange={this.handleChange}/>
+                    <input 
+                        type="text" 
+                        name="submitedStrength" 
+                        id="strength" 
+                        defaultValue={this.props.gnomeStrength} 
+                        onChange={this.handleChange}/>
                 </div>
                 {this.state.validationMessage ? <p className="message" style={validationMessageColor}>{this.state.validationMessage}</p> : null}
                 <button type="submit" className="submit-btn">Submit</button>
